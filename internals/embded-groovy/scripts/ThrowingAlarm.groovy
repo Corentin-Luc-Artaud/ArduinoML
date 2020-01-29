297bea592036b93dc2a3e169ac7625ee4133c9f2
Theos <theos.mariani@gmail.com>
2020-01-29 09:07:22 +0100

    add throwing scénario

---
+++ internals/embded-groovy/scripts/ThrowingAlarm.groovy
@@ -0,0 +1,21 @@
+sensor but1 pin 9
+sensor but2 pin 10
+actuator led1 pin 11
+actuator led2 pin 12
+
+state iddle means led1 becomes low and led2 becomes low
+state led1_on means led1 becomes high
+state throwing means led2 becomes throwing
+
+initial iddle
+
+from iddle to led1_on when but1 becomes high
+from iddle to led1_on when but2 becomes high
+
+from led1_on to iddle when but1 becomes low
+from led1_on to iddle when but2 becomes low
+
+from led1_on to throwing when but1 becomes high and but2 becomes high
+from iddle to throwing when but1 becomes high and but1 becomes high
+
+export "ThrowingAlarm!"
